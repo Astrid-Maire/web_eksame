9d4c887f8ace6f00163463f12c1605cf0994ad05
Astrid Marie Lindholm <asli0001@stud.kea.dk>
2024-01-09 22:12:24 +0100

    first commit

---
+++ javascript.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const burgerIcon = document.getElementById("burger-icon");
+  const menu = document.getElementById("menu");
+
+  burgerIcon.addEventListener("click", function () {
+    menu.classList.toggle("active");
+  });
+});
